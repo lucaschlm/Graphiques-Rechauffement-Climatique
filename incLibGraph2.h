7abfe44892ef6c5beb39113cff6967fdd8ce8e92
Lucas Chalmandrier <lucas.chalmandrier@etu.uca.fr>
2022-05-04 10:43:54 +0200

    Ajout libgraph

---
+++ incLibGraph2.h
@@ -0,0 +1,18 @@
+/*!
+ * \file "incLibGraph2.h"
+ * 
+ * \version 2016
+ *
+ * \brief Fichier d'inclusion de la bibliothèque LibGraph2.
+ *
+ * Ce fichier permet de centraliser l'inclusion de la bibliothèque LibGraph2 
+ * pour s'assurer que le niveau utilisé de la bibliothèque est bien le même 
+ * quelques soient les fichiers sources.
+ *
+ */
+#pragma once
+/*!\brief Défini le niveau d'utilisation de LibGraph2 au niveau 3
+ */
+#define LIBGRAPH2_LEVEL 4
+#include <LibGraph2.h>
+
--- mainLectureData.cpp
+++ mainLectureData.cpp
@@ -1,15 +1,49 @@
-#include <iostream> // std::cout
-#include "SAE_Datas_Heat.h"
-#include "Traitement.h"
+#include <Windows.h>
+#include <tchar.h>
+#include "incLibGraph2.h"
+using namespace LibGraph2;
 
-
-
-int main()
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPTSTR    lpCmdLine,
+    int       nCmdShow)
 {
-	SAE_Datas_Heat myStations;
-	std::cout << myStations.nbMonths() << "\n";
-	//ListeStation lB = myStations.getListeStation();
-	
-	return 0;
-}
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
+    //Récupération de l'objet principal de LibGraph 2
+    ILibGraph2* libgraph = GetLibGraph2();
+    //Affiche la fenêtre graphique avec une taille par défaut
+    libgraph->show();
+    evt e;  //Evénement LibGraph 2
+
+    //Boucle principale d'événements
+    while (libgraph->waitForEvent(e))
+    {
+        switch (e.type)  //En fonction du type d'événement
+        {
+        case evt_type::evtRefresh:  //Raffraichissement de l'affichage (tout redessiner)
+          //Utiliser éventuellement les fonctions libgraph->beginPaint() / libgraph->endPaint() pour activer le backbuffer
+            libgraph->beginPaint();
+            //Utiliser ici les fonctions de dessins
+            libgraph->endPaint();
+            break;
+
+        case evt_type::evtKeyDown:  //Enfoncement d'une touche
+            switch (e.vkKeyCode) //En fonction de la touche enfoncée
+            {
+            case 'A':
+                break;
+            }
+            break;
+
+        case evt_type::evtMouseMove:  //Déplacement de la souris
+          //Position de la souris dans les variables e.x et e.y
+            break;
+        }
+    }
+
+    //Libération de l'objet principal de LibGraph 2
+    ReleaseLibGraph2();
+    return 0;
+}
