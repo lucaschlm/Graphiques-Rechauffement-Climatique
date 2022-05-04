dd26841cdc914fc6042439962e4fd9a322ce3174
Lucas Chalmandrier <lucas.chalmandrier@etu.uca.fr>
2022-05-04 16:43:13 +0200

    Organisation du code

--- fonctionsLibgraph.h
+++ fonctionsLibgraph.h
@@ -1,4 +1,8 @@
 #pragma once
 #include <vector>
 
+const float pi = 3.14159265358979323846;
+
 void displayClairFonce(const std::vector<float>& data, size_t colorNb = 8);
+
+void displaySpirale(const std::vector<float>& sommeMoyStation);
