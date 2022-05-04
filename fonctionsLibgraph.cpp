8f8f3911d9e2618e2705b2e94186cec947279b2c
Lucas Chalmandrier <lucas.chalmandrier@etu.uca.fr>
2022-05-04 15:48:19 +0200

    Essaie d'ajout d'une légende (non fonctionnel)

--- fonctionsLibgraph.cpp
+++ fonctionsLibgraph.cpp
@@ -6,17 +6,17 @@
 using namespace LibGraph2;
 
 /*!
- *  \brief  Fonction permettant d'afficher une représentation Clair-Foncé des températures
- *  \param  data    - tableau contenant les températures
- *  \param  colorNb - Le nombre de couleurs différentes sur la représentation (max=8)
+ *  \brief  Fonction permettant d'afficher une reprÃ©sentation Clair-FoncÃ© des tempÃ©ratures
+ *  \param  data    - tableau contenant les tempÃ©ratures
+ *  \param  colorNb - Le nombre de couleurs diffÃ©rentes sur la reprÃ©sentation (max=8)
  *  \author Lucas Chalmandrier
  */
 void displayClairFonce(const std::vector<float>& data, size_t colorNb)
 {
-    //Tableau contenant les couleurs de la représentation (du plus chaud au plus froid)
+    //Tableau contenant les couleurs de la reprÃ©sentation (du plus chaud au plus froid)
     std::array<ARGB, 8> colors{ MakeARGB(255, 3, 4, 94), MakeARGB(255, 2, 62, 138), MakeARGB(255, 0, 180, 216), MakeARGB(255, 173, 232, 244),
     MakeARGB(255, 249, 237, 204), MakeARGB(255, 230, 96, 99), MakeARGB(255, 208, 34, 36), MakeARGB(255, 156, 25, 27) };
-    //On calcule les intervalles de températures
+    //On calcule les intervalles de tempÃ©ratures
     float maxTemp = *std::max_element(data.begin(), data.end());
     float minTemp = *std::min_element(data.begin(), data.end());
     float deltaTemp = maxTemp - (minTemp - 1);
@@ -28,33 +28,37 @@ void displayClairFonce(const std::vector<float>& data, size_t colorNb)
         intervals[i] = minTemp + interval * i;
 
 
-    //Récupération de l'objet principal de LibGraph 2
+    //RÃ©cupÃ©ration de l'objet principal de LibGraph 2
     ILibGraph2* libgraph = GetLibGraph2();
-    //Affiche la fenêtre graphique avec une taille par défaut
+    //Affiche la fenÃªtre graphique avec une taille par dÃ©faut
     libgraph->show();
-    evt e;  //Evénement LibGraph 2
+    evt e;  //EvÃ©nement LibGraph 2
 
-    //On récupère la taille de la fenêtre
+    //On rÃ©cupÃ¨re la taille de la fenÃªtre
     float width = libgraph->getSize().m_fWidth;
     float height = libgraph->getSize().m_fHeight;
 
-    //On calcule une taille pour la marge pour laisser de la place à la légende
+    //On calcule une taille pour la marge pour laisser de la place Ã  la lÃ©gende
     float widthMargin = libgraph->getSize().m_fWidth * (10.f / 100.f);
-    float heightMargin = libgraph->getSize().m_fHeight * (5.f / 100.f);
+    float heightMargin = libgraph->getSize().m_fHeight * (30.f / 100.f);
 
     //On calcule la largeur d'un rectangle (12 mois sur la largeur)
     float rectangleWidth = (width - widthMargin) / 12;
     //Puis on calcul la hauteur (un rectangle par an)
     float rectangleHeight = (height - heightMargin) / (data.size() / 12);
 
-    //Boucle principale d'événements
+    //Boucle principale d'Ã©vÃ©nements
     while (libgraph->waitForEvent(e))
     {
-        switch (e.type)  //En fonction du type d'événement
+        switch (e.type)  //En fonction du type d'Ã©vÃ©nement
         {
         case evt_type::evtRefresh:  //Raffraichissement de l'affichage (tout redessiner)
-          //Utiliser éventuellement les fonctions libgraph->beginPaint() / libgraph->endPaint() pour activer le backbuffer
+          //Utiliser Ã©ventuellement les fonctions libgraph->beginPaint() / libgraph->endPaint() pour activer le backbuffer
             libgraph->beginPaint();
+
+            //On affiche le nom du graphique en haut Ã  gauche
+            //libgraph->setFont(CString("Consolas"), 12, font_styles::FontStyleRegular);
+            //libgraph->drawString(CString("Clair-Fonce"), CPoint(0, 0));
 
             size_t lineNb = 0; //numero de ligne
             size_t colNb = 0; //numero de colonne
@@ -62,7 +66,7 @@ void displayClairFonce(const std::vector<float>& data, size_t colorNb)
             //On parcourt toutes les valeurs
             for (size_t i = 0; i < data.size(); i++)
             {
-                if (i % 12 == 0 && i != 0) // Si on change d'année (12mois) on change de ligne
+                if (i % 12 == 0 && i != 0) // Si on change d'annÃ©e (12mois) on change de ligne
                 {
                     lineNb++;
                     colNb = 0;
@@ -78,7 +82,7 @@ void displayClairFonce(const std::vector<float>& data, size_t colorNb)
                     }
                 }
                 libgraph->setSolidBrush(color);
-                CRectangle rectangle(CPoint(colNb * rectangleWidth + widthMargin, lineNb * rectangleHeight + heightMargin), CSize(rectangleWidth, rectangleHeight));
+                CRectangle rectangle(CPoint(colNb * rectangleWidth + widthMargin, lineNb * rectangleHeight), CSize(rectangleWidth, rectangleHeight));
                 libgraph->drawRectangle(rectangle);
                 colNb++;
             }
@@ -88,7 +92,7 @@ void displayClairFonce(const std::vector<float>& data, size_t colorNb)
         }
     }
 
-    //Libération de l'objet principal de LibGraph 2
+    //LibÃ©ration de l'objet principal de LibGraph 2
     ReleaseLibGraph2();
 
 }
